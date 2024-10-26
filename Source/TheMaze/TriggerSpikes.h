35c9d9cd9a93746afa698a2b1aec4383ef88cc8a
gabriel_combe <gabrielcombe@gmail.com>
2024-10-26 22:26:49 +0200

    Add trigger spikes c++ class (WIP)

---
+++ Source/TheMaze/TriggerSpikes.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "TriggerSpikes.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEMAZE_API ATriggerSpikes : public ATriggerBox
+{
+	GENERATED_BODY()
+	
+};

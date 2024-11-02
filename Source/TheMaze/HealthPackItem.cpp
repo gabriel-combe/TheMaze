2f527815c20797406c55817fbe036bed34976d04
gabriel_combe <gabrielcombe@gmail.com>
2024-11-02 04:43:29 +0100

    Add test healthpack item with implementation of the interactable interface (WIP)

---
+++ Source/TheMaze/HealthPackItem.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPackItem.h"
+
+// Sets default values
+AHealthPackItem::AHealthPackItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	HealthPack = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HealthPack"));
+	HealthPack->SetupAttachment(RootComponent);
+	//HealthPack->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
+	//HealthPack->SetGenerateOverlapEvents(false);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereDefault(TEXT("/Engine/BasicShapes/Sphere.Sphere"));
+	if (SphereDefault.Succeeded()) {
+		HealthPack->SetStaticMesh(SphereDefault.Object);
+		HealthPack->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+		HealthPack->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+	}
+
+}
+
+// Called when the game starts or when spawned
+void AHealthPackItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHealthPackItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Implement the Interact function of the interface
+void AHealthPackItem::Interact()
+{
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Emerald, "HEALTHPACK INCOMING");
+
+	Destroy(true);
+}
---
+++ Source/TheMaze/HealthPackItem.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "GameFramework/Actor.h"
+#include "HealthPackItem.generated.h"
+
+
+UCLASS()
+class THEMAZE_API AHealthPackItem : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Mesh, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* HealthPack;
+	
+public:	
+	// Sets default values for this actor's properties
+	AHealthPackItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Implement the Interact function of the interface
+	virtual void Interact() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
